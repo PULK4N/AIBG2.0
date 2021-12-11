de54c3faa17ac209f043602964030a1d0f10299d
Dejan Tosenberger <tosemberger1@gmail.com>
2021-12-11 21:08:15 +0100

    Fix

--- Source/AIBG20/Command/BuyingLandActionCommand.cpp
+++ Source/AIBG20/Command/BuyingLandActionCommand.cpp
@@ -44,22 +44,22 @@ bool BuyingLandActionCommand::TileAlreadyOwned() {
 }
 
 bool BuyingLandActionCommand::IsNeighbourTile() {
-	if (CoordinationX < 0)
+	if (CoordinationX < 0 || CoordinationX > 7)
 		return false;
-	if (CoordinationY < 0)
+	if (CoordinationY < 0 || CoordinationY > 7)
 		return false;
 	
 	for (int i = -1; i <= 1; i++) {
-		if (CoordinationX == 0 && i == -1) continue;
+		if ((CoordinationX == 0 && i == -1) || (CoordinationX == 7 && i == 1)) continue;
 		for (int j = -1; j <= 1; j++) {
-			if (CoordinationY == 0 && j == -1) continue;
+			if ((CoordinationY == 0 && j == -1) || (CoordinationY == 7 && j == 1)) continue;
 			ATile* tile = GameMap->FindTile(CoordinationX + i, CoordinationY + j);
-			if (Player->Name == GameMap->Player1->Name) {
+			if (Player == GameMap->Player1) {
 				if (GameMap->Player1->Tiles.Find(tile) != INDEX_NONE) {
 					return true;
 				}
 			}
-			if (Player->Name == GameMap->Player2->Name) {
+			if (Player == GameMap->Player2) {
 				if (GameMap->Player2->Tiles.Find(tile) != INDEX_NONE) {
 					return true;
 				}
