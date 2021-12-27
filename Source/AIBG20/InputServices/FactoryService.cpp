5ed1f67b2302332f5dcbd8e3a65f2fda09446997
andjmiskovic <andjelamiskovic2000@gmail.com>
2021-12-27 16:35:54 +0100

    almost done InputService

--- Source/AIBG20/InputServices/FactoryService.cpp
+++ Source/AIBG20/InputServices/FactoryService.cpp
@@ -7,3 +7,25 @@ FactoryService::FactoryService()
 FactoryService::~FactoryService()
 {
 }
+
+ActionCommandFactory* FactoryService::InputAction(FString action, AGamePlayer *player) {
+    switch (action.at(0))
+    {
+    case 'W':
+        return new WateringCommandFactory(); 
+    case 'C':
+        return new BuyingCommandFactory(); 
+    case 'P':
+        return new PlantingCommandFactory(); 
+    case 'F':
+        return new FertilizerCommandFactory(); 
+    case 'M':
+        return new MoleCommandFactory(); 
+    case 'H':
+        return new HarvestingCommandFactory(); 
+    case 'L':
+        return new BuyingLandCommandFactory();    
+    default:
+        break;
+    }
+}
--- Source/AIBG20/InputServices/InputService.cpp
+++ Source/AIBG20/InputServices/InputService.cpp
@@ -2,16 +2,14 @@
 
 void InputService::SendCommand(FString action, AGamePlayer *source)
 {
- //   if(this.lastPlayer == source) 
- //       return;
- //   if(!this.timerService.TimerFinished())
- //       return;
- //   this.timerService.StartTimer(WAIT_TIME);
- //   this.lastPlayer = source;
+    if(this.lastPlayer == source) 
+        return;
+    if(!this.timerService.TimerFinished())
+        return;
+    this.timerService.StartTimer(WAIT_TIME);
+    this.lastPlayer = source;
     // update turns in GameMap
-    // create factory
-//	this->factoryService.actionCommandFactory.CreateActionCommand(action, source);
-    // create commands from factory
-    // send them to action service
-
+    this->factoryService = new FactoryService();
+	this->factoryService.CreateActionCommand(action, source);
+    this.actionService.ExecuteActions(actionCommands, source);
 }
