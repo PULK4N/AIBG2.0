d7074784c93a03f60c737c487af4088d9719d846
andjmiskovic <andjelamiskovic2000@gmail.com>
2021-12-29 22:53:23 +0100

    Added OutputService

---
+++ Intermediate/ProjectFiles/OutputService.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OutputService.h"
+#include "Json.h"
+
+
+int** AOutputService::generateMap(AGameMap* gm)
+{
+	int** map = new int* [8];
+	for (int i = 0; i < 8; ++i)
+	{
+		map[i] = new int[8];
+
+		for (int j = 0; j < 8; ++j)
+		{
+			map[i][j] = gm->WhoOwnesTile(i, j);
+		}
+	}
+	return map;
+}
+
+void AOutputService::setParams(AGameMap* gm, AGamePlayer* gp)
+{
+	PlayerSource = gp;
+	TileMap = generateMap(gm);
+	gold = gp->Gold;
+	points = gp->Points;
+	fertilizerActive = gp->fertilizerActive;
+	bRainedThisTurn = gm->turn % 10;
+	for (ACard* c : gp->Cards) {
+		cards.insert(make_pair(c->id, c->owned));
+	}
+}
+/*
+Fstring AOutputService::convertToJSON(AGameMap* gm, AGamePlayer* gp)
+{
+	setParams(gm, gp);
+	FString JsonRaw = "{ \"exampleString\": \"Hello World\" }";
+	TSharedPtr<FJsonObject> JsonParsed;
+	TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(JsonRaw);
+	if (FJsonSerializer::Deserialize(JsonReader, JsonParsed))
+	{
+		FString ExampleString = JsonParsed->GetStringField("exampleString");
+	}
+	return 
+}*/
+
+// Sets default values
+AOutputService::AOutputService()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AOutputService::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AOutputService::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
