3802066ae5b6ee57fbb824ca35410ec6c3e22e35
PULK4N <nikola_s99@live.com>
2021-12-30 16:25:32 +0100

    fixed error

--- Source/AIBG20/Service/OutputService.cpp
+++ Source/AIBG20/Service/OutputService.cpp
@@ -1,9 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "OutputService.h"
-#include "Json.h"
-#include <map>
 
+#include "OutputService.h"
+#include "../Entity/GameMap.h"
+#include "../Entity/GamePlayer.h"
+
+// Sets default values
+AOutputService::AOutputService()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
 
 int** AOutputService::generateMap(AGameMap* gm)
 {
@@ -22,43 +30,22 @@ int** AOutputService::generateMap(AGameMap* gm)
 
 void AOutputService::setParams(AGameMap* gm, AGamePlayer* gp)
 {
-	PlayerSource = gp;
-	TileMap = generateMap(gm);
-	gold = gp->Gold;
-	points = gp->Points;
-	fertilizerActive = gp->fertilizerActive;
-	bRainedThisTurn = gm->turn % 10;
-	for (ACard* c : gp->Cards) {
-		cards.insert(make_pair(c->id, c->owned));
-	}
-}
-/*
-Fstring AOutputService::convertToJSON(AGameMap* gm, AGamePlayer* gp)
-{
-	setParams(gm, gp);
-	FString JsonRaw = "{ \"exampleString\": \"Hello World\" }";
-	TSharedPtr<FJsonObject> JsonParsed;
-	TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(JsonRaw);
-	if (FJsonSerializer::Deserialize(JsonReader, JsonParsed))
-	{
-		FString ExampleString = JsonParsed->GetStringField("exampleString");
-	}
-	return 
-}*/
-
-// Sets default values
-AOutputService::AOutputService()
-{
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
-
+	//PlayerSource = gp;
+	//TileMap = generateMap(gm);
+	//gold = gp->Gold;
+	//points = gp->Points;
+	//fertilizerActive = gp->fertilizerActive;
+	//bRainedThisTurn = gm->turn % 10;
+	//for (ACard* c : gp->Cards) {
+	//	cards.insert(make_pair(c->id, c->owned));
+	//}
 }
 
 // Called when the game starts or when spawned
 void AOutputService::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 }
 
 // Called every frame
@@ -67,3 +54,4 @@ void AOutputService::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
--- Source/AIBG20/Service/OutputService.h
+++ Source/AIBG20/Service/OutputService.h
@@ -1,21 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "OutputService.generated.h"
-#include "Dom/JsonObject.h"
 
-class AGamePlayer;
 class AGameMap;
+class AGamePlayer;
 
 UCLASS()
 class AIBG20_API AOutputService : public AActor
 {
 	GENERATED_BODY()
-
-public:
+	
+public:	
 	// Sets default values for this actor's properties
 	AOutputService();
+
 	//string convertToJSON(AGameMap* gm, AGamePlayer* gp);
 	int** generateMap(AGameMap* gm);
 	void setParams(AGameMap* gm, AGamePlayer* gp);
@@ -32,9 +34,9 @@ protected:
 	bool bRainedThisTurn;
 	int gold;
 	int points;
-	TMap<int, int> cards;
+	TMap<int, int> info;
 
-public:
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
